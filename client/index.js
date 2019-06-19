0074996f8f7ad0ddefe9248027891c766e90d038
Cassie Rosner <cerosner@gmail.com>
2019-06-18 20:36:43 -0400

    all-contacts ajax request through hooks

--- client/index.js
+++ client/index.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import ContactList from './ContactList'
 
 function Main() {
   const [contacts, setContacts] = useState([])
+
+  function fetchData() {
+    return axios.get('/api/contacts')
+    .then(res => res.data)
+    .catch(()=> console.log(`Couldn't fetch :(`))
+  }
+
+  useEffect(() => {
+    fetchData()
+    .then(res => setContacts(res))
+    .catch(() => `Something's wrong in component lifecycle!`)
+  }, [])
 
   return (
     <div id='main'>
@@ -11,7 +24,7 @@ function Main() {
         <div>Contact List</div>
       </div>
       <div id='container'>
-        <ContactList contacts={contacts}/>
+        <ContactList contacts={contacts} />
       </div>
     </div>
   )
